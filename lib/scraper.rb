c614c586507fefa3a02aed572c546b41f9f77612
Blaine Pace <blainepace@gmail.com>
2012-05-22 23:39:00 -0700

    Persistent thread scraping
    Rubychan now persistently scrapes a 4chan thread until it 404's.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,20 +6,49 @@ module RubyChan
 
     def initialize(uri)
       @uri = uri
+      @images = Hash[]
     end
 
     def scrape
-      doc = Nokogiri::HTML(open(@uri))
-      doc.xpath('//a').each do |link|
+      loop do
+        begin
+          parse_page
+          download
+        rescue OpenURI::HTTPError
+          puts "Error, thread has 404'd"
+          break
+        end
+      end
+    end
+
+    private
+
+    def parse_page
+     doc = Nokogiri::HTML(open(@uri))
+     doc.xpath('//a').each do |link|
         if(link['href'] =~ /\/\/images.4chan.org\/.+\/src\/\d+\..../)
           uri = URI(link['href'])
-          puts "Downloading #{uri}"
-          Net::HTTP.start(uri.host) do |http|
-            resp = http.get(uri.path)
-            open(File.basename(uri.path), "wb") do |file|
+          # Mark image as not downloaded yet in hash
+          unless @images.has_key?(uri)
+            @images[uri] = false
+          end
+        end
+      end
+    end
+
+    def download
+      @images.each_key do |image|
+        if @images[image] == false
+          Net::HTTP.start(image.host) do |http|
+            resp = http.get(image.path)
+            open(File.basename(image.path), "wb") do |file|
               file.write(resp.body)
             end
           end
+          puts "Downloaded #{image} to disk."
+          # Mark image as downloaded so it isn't downloaded again in 
+          # subsequent loops
+          @images[image] = true
         end
       end
     end
